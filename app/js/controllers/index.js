e75bdfdc2d5801818b34946e0de42f4564e51d4b
Katherine Beame <katherine.beame@gmail.com>
2016-11-29 15:28:05 -0800

    try3

--- app/js/controllers/index.js
+++ app/js/controllers/index.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(app) {
-  require('./dropController')(app);
+  require('./dropController.js')(app);
 };
--- app/js/directive/index.js
+++ app/js/directive/index.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(app) {
-  require('./obesityChartDirective')(app);
+  require('./obesityChartDirective.js')(app);
 };
