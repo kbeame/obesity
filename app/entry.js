278c6b09e844cce88d6ed75901fcaab250d648b8
Katherine Beame <katherine.beame@gmail.com>
2016-11-29 15:35:50 -0800

    added csv to build

--- app/entry.js
+++ app/entry.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require('!!file?name=[name].[ext]!./index.html');
+require('!!file?name=[name].[ext]!./only-obese-data.csv');
 require('./scss/base.scss');
 
 const angular = require('angular');
